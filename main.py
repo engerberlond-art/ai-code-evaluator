eace8e25ab1a20a953e7737c16470b773eb20faf
engerberlond-art <engerberlond@gmail.com>
2026-08-19 02:55:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import time
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CodeData(BaseModel):
+    code: str
+
+@app.get("/")
+def home():
+    return {"message": "AI Code Evaluator is running successfully!"}
+
+@app.post("/analyze")
+def analyze_code(data: CodeData):
+    start_time = time.time()
+    try:
+        exec(data.code)
+        execution_time = time.time() - start_time
+        return {
+            "status": "Success",
+            "execution_time": f"{execution_time:.4f} seconds",
+            "message": "Code executed successfully."
+        }
+    except Exception as e:
+        return {
+            "status": "Error",
+            "message": str(e)
+        }
 
